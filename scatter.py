f6c14b74a7e5d8f5c128d5e577af7d0d65885600
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2017-08-07 10:59:46 -0300

    Colors addition

--- scatter.py
+++ scatter.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 iris = pd.read_csv('iris.csv')
-plt.scatter(iris['sepal.length'], iris['sepal.width'], sizes=50 * iris['petal.length'])
+plt.scatter(iris['sepal.length'], iris['sepal.width'], sizes=50 * iris['petal.length'],c=iris['petal.width'], cmap='viridis', alpha=0.4)
 
 
 
